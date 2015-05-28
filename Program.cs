8c964a9e62faf4cf04684a3668857ff40a03845b
John Shahid <jvshahid@gmail.com>
2015-05-28 09:42:21 -0400

    Init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = "/C set",
+                RedirectStandardOutput = true,
+                UseShellExecute = true,
+            };
+            p.Start();
+            p.WaitForExit(1000);
+            if (p.HasExited)
+            {
+                Console.WriteLine(p.ExitCode);
+                Console.WriteLine(p.StandardOutput.ReadToEnd());
+            }
+            else
+            {
+                Console.WriteLine("DID NOT EXIT");
+            }
+        }        
+    }
+}
